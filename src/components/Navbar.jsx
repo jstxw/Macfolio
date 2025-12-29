b9dffa21c734fc0088d2a3778f7e826231135447
jsxtw <justintwx7@gmail.com>
2025-12-28 20:57:31 -0500

    added utility functions, index.css, imports, aliases, and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { navLinks, navIcons } from "../constants";
+import dayjs from "dayjs";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Justin's Portfolio</p>
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+        {/*avoids repetitive code, handles dynamic lists*/}
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => {
+            return (
+              <li key={id}>
+                <img src={img} className="icon-hover" alt={`icon-${id}`} />
+              </li>
+            );
+          })}
+        </ul>
+
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
